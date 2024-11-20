774c1418752b43adfd53f23a2c361bfccb7ad8e8
JuKpe <149210632+JuKpe@users.noreply.github.com>
2024-11-20 20:36:56 +0000

    Update

--- pages/3_prueba.py
+++ pages/3_prueba.py
@@ -3,3 +3,15 @@ from ucimlrepo import fetch_ucirepo, list_available_datasets
 
 st.write("# Welcome to prueba! 👋")
 
+# check which datasets can be imported
+list_available_datasets()
+
+# import dataset
+heart_disease = fetch_ucirepo(id=45)
+# alternatively: fetch_ucirepo(name='Heart Disease')
+
+# access data
+X = heart_disease.data.features
+y = heart_disease.data.targets
+
+X
