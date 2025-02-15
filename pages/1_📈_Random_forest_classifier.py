b7f3beda67b6db74b0b04218d73c8b74bf081e46
Juan Carlos <149210632+JuKpe@users.noreply.github.com>
2025-02-15 21:39:09 +0000

    Update

--- pages/1_📈_Random_forest_classifier.py
+++ pages/1_📈_Random_forest_classifier.py
@@ -7,31 +7,13 @@ from sklearn.ensemble import RandomForestClassifier
 st.set_page_config(page_title="Random forest classifier", page_icon="📈")
 
 st.markdown("# Random forest classifier")
-st.sidebar.header("Random forest classifier hola")
 st.write(
     """En este apartado se muestra el uso del modelo Random forest de clasificación"""
 )
 
+st.sidebar.header("Hola")
 st.sidebar.success("Seleccione un modelo.")
 
-
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
 
 with st.expander('DATA'):
   st.write('**Raw Data**')
